88aa003bf77c280ee4647eaefe8376bb5842d439
anouar garbaa <g.anouar@yahoo.com>
2024-05-10 12:39:58 +0100

    'ajout de useEaffect'

--- src/App.js
+++ src/App.js
@@ -1,58 +1,55 @@
-import Content from "./Content";
-import Footer from "./Footer";
-import Header from "./Header";
-import SearchItem from "./SearchItem";
-import AddItem from "./AddItem";
-import { useState } from "react";
+import Header from './Header';
+import SearchItem from './SearchItem';
+import AddItem from './AddItem';
+import Content from './Content';
+import Footer from './Footer';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist"))
-  );
-  const [newItem, setNewItem] = useState("");
-  const [search, setSearch] = useState("");
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+  const [newItem, setNewItem] = useState('')
+  const [search, setSearch] = useState('')
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
-  };
+  useEffect(() => {
+    localStorage.setItem('shoppinglist', JSON.stringify(items));
+  }, [items])
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
-  };
+    setItems(listItems);
+  }
 
   const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setAndSaveItems(listItems);
-  };
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+  }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
-  };
+    setItems(listItems);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!newItem) return; //checks if newItem is empty or falsy. If it is, the function exits immediately using return;
+    if (!newItem) return;
     addItem(newItem);
-    setNewItem("");
-  };
+    setNewItem('');
+  }
 
   return (
     <div className="App">
-      <Header title="Groceries" />
-      
+      <Header title="Grocery List" />
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-      <SearchItem search={search} setSearch={setSearch} />
+      <SearchItem
+        search={search}
+        setSearch={setSearch}
+      />
       <Content
         items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
